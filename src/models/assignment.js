1a6e393525fe61aea44a274b1a260be60b8d7d3d
Ahmet Faruk ASLANTAŞ <ahmetf.aslantas@gmail.com>
2022-03-31 00:38:38 +0300

    Fixed linting issues, modified models.

--- src/models/assignment.js
+++ src/models/assignment.js
@@ -1,27 +1,30 @@
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const Assignment = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-    default: "",
-  },
-  dueDate: {
-    type: Date,
-    required: true,
-  },
-  course: {
-    type: Schema.Types.ObjectId,
-    ref: "Course",
-    required: true,
-  },
-  files: {
-    type: Array,
-    default: [],
-  },
+const assignmentSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        default: "",
+    },
+    dueDate: {
+        type: Date,
+        required: true,
+    },
+    course: {
+        type: Schema.Types.ObjectId,
+        ref: "Course",
+        required: true,
+    },
+    files: {
+        type: Array,
+        default: [],
+    },
 });
 
-export default mongoose.model("Assignment", Assignment);
+const Assignment = mongoose.model("Assignment", assignmentSchema);
+
+exports.Assignment = Assignment;
--- src/models/course.js
+++ src/models/course.js
@@ -1,33 +1,36 @@
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const Course = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-    default: "",
-  },
-  instructor: {
-    type: Schema.Types.ObjectId,
-    ref: "Instructor",
-    required: true,
-  },
-  students: {
-    type: Schema.Types.ObjectId,
-    ref: "Student",
-    default: [],
-  },
-  assignments: {
-    type: Schema.Types.ObjectId,
-    ref: "Assignment",
-    default: [],
-  },
-  files: {
-    type: Array,
-    default: [],
-  },
+const courseSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        default: "",
+    },
+    instructor: {
+        type: Schema.Types.ObjectId,
+        ref: "Instructor",
+        required: true,
+    },
+    students: {
+        type: Schema.Types.ObjectId,
+        ref: "Student",
+        default: [],
+    },
+    assignments: {
+        type: Schema.Types.ObjectId,
+        ref: "Assignment",
+        default: [],
+    },
+    files: {
+        type: Array,
+        default: [],
+    },
 });
 
-export default mongoose.model("Course", Course);
+const Course = mongoose.model("Course", courseSchema);
+
+exports.Course = Course; 
