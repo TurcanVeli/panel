8bf44f7342267a0e08a56c55f581761323db3e75
nuricann44 <86882936+nuricann44@users.noreply.github.com>
2022-04-03 16:44:43 +0300

    Added backend routes for the registration and login procedures. Scripts are added to test the server.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,30 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const userRouter = require("./routes/userRoutes");
 
 const app = express();
 
 const port = process.env.PORT || 5000;
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-});
+app.use(express.json());
+app.use(cors());
+
+console.log("Connecting to database...");
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Connected to database");
+  })
+  .catch((err) => {
+    console.log("Error connecting to database" + err);
+    console.log("Exiting...");
+    process.exit(1);
+  });
+
+app.use("/api/auth", userRouter);
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
---
+++ src/routes/userRoutes.js
@@ -0,0 +1,83 @@
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const { User } = require("../models/user");
+const router = require("express").Router();
+require("dotenv").config();
+
+router.post("/register", async (req, res) => {
+  try {
+    const { name, email, password, isInstructor } = req.body;
+
+    if (
+      name === "" ||
+      email === "" ||
+      password === "" ||
+      typeof isInstructor === "undefined"
+    ) {
+      return res.status(400).json({ msg: "Please enter all fields" });
+    }
+
+    const oldUser = await User.findOne({ email: email });
+
+    if (oldUser) {
+      return res.status(400).json({ msg: "User already exists" });
+    }
+
+    const passwordHash = bcrypt.hashSync(password, 10);
+
+    const user = new User({
+      name,
+      email,
+      password: passwordHash,
+      isInstructor,
+    });
+
+    const token = jwt.sign({ userId: user._id }, process.env.TOKEN_SECRET, {
+      expiresIn: "1h",
+    });
+
+    await user.save();
+
+    res
+      .status(201)
+      .cookie("token", token)
+      .json({ message: "User created successfully" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (email === "" || password === "") {
+      return res.status(400).json({ msg: "Please enter all fields" });
+    }
+
+    const user = await User.findOne({ email: email });
+
+    if (!user) {
+      return res.status(400).json({ msg: "User does not exist" });
+    }
+
+    const isMatch = bcrypt.compareSync(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({ msg: "Wrong password" });
+    }
+
+    const token = jwt.sign({ userId: user._id }, process.env.TOKEN_SECRET, {
+      expiresIn: "1h",
+    });
+
+    res
+      .status(200)
+      .cookie("token", token)
+      .json({ message: "User logged in successfully" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+module.exports = router;
